71b2dff2ec9818a21ee5d2ce582336e3ae45cb0a
kfzhu <kfzhu@803ee233-1bbf-4b9d-9566-d4e45d14bbbd>
2017-12-04 04:36:29 +0000

    git-svn-id: http://47.100.187.235:13932/svn/datacenter@151 803ee233-1bbf-4b9d-9566-d4e45d14bbbd

--- src/main/webapp/resources/media/admin/plugins/beyond/js/datetime/bootstrap-datepicker.js
+++ src/main/webapp/resources/media/admin/plugins/beyond/js/datetime/bootstrap-datepicker.js
@@ -21,12 +21,12 @@
 	
 	// Picker object
 	
-	var Datepicker = function(element, options,container){
+	var Datepicker = function(element, options,scrollEle){
 		this.element = $(element);
-		this.container = container;
+		this.scrollEle = scrollEle? $(scrollEle) : $('body');  //所在的滚动层
 		this.format = DPGlobal.parseFormat(options.format||this.element.data('date-format')||'mm/dd/yyyy');
 		this.picker = $(DPGlobal.template)
-							.appendTo(container)
+							.appendTo('body')
 							.on({
 								click: $.proxy(this.click, this)//,
 								//mousedown: $.proxy(this.mousedown, this)
@@ -37,8 +37,8 @@
 		if (this.isInput) {
 			this.element.on({
 				focus: $.proxy(this.show, this),
-				blur: $.proxy(this.hide, this),
-				keyup: $.proxy(this.update, this)
+				//blur: $.proxy(this.hide, this),
+				keyup: $.proxy(this.update, this),
 			});
 		} else {
 			if (this.component){
@@ -83,25 +83,47 @@
 		this.fillDow();
 		this.fillMonths();
 		this.update();
-		this.showMode();
+        this.showMode();
 	};
 	
 	Datepicker.prototype = {
 		constructor: Datepicker,
 		
 		show: function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			var triggerEle = this.component ? this.component : this.element;
+			var PICKER = this.picker;
 			this.picker.show();
 			this.height = this.component ? this.component.outerHeight() : this.element.outerHeight();
+			var EleHeight = this.height; //触发元素的高度
 			this.place();
 			$(window).on('resize', $.proxy(this.place, this));
-			if (e ) {
-				e.stopPropagation();
-				e.preventDefault();
-			}
 			if (!this.isInput) {
 			}
 			var that = this;
 			$(document).on('mousedown', function(ev){
+				$('input').blur();
+				if ($(ev.target).closest('.datepicker').length == 0) {
+					that.hide();
+				}
+            });
+            $(this.scrollEle).on('scroll', function(){
+            	console.log('scroll');
+                var offset = triggerEle.offset();
+                //判断生成的picker 是在触发元素的上方还是下方
+                if(PICKER.hasClass('bottom')){
+                	PICKER.css({
+                        top: offset.top - 232
+                    })
+                }else {             
+                    PICKER.css({
+                        top: offset.top + EleHeight,
+                    })
+                }
+                
+			});
+			$(document).on('', function(ev){
 				if ($(ev.target).closest('.datepicker').length == 0) {
 					that.hide();
 				}
@@ -151,17 +173,24 @@
 		},
 		
 		place: function(){
-			var ELEMENT = this.component ? this.component : this.element;
-			var offset = this.component ? this.component.offset() : this.element.offset();
-			var top = this.component ? this.component[0].offsetTop : this.element[0].offsetTop;
-			var left = this.component ? this.component[0].offsetLeft : this.element[0].offsetLeft;
-
-			this.picker.css({
-				top: top + this.height,
-				left: left
-			});
-
 			
+			var windowHeight = $(window).height(); 
+			var offset = this.component ? this.component.offset() : this.element.offset();
+			var bottomHeight = windowHeight - offset.top - this.height;  //触发元素距离底部的距离
+			var	pickerLeft = offset.left;
+			var pickerTop;
+			
+			if( bottomHeight > 240 ){
+				pickerTop = offset.top + this.height;	
+				this.picker.removeClass('bottom');
+			}else {
+				pickerTop = offset.top - 232;
+				this.picker.addClass('bottom');
+			}
+			this.picker.css({
+				top: pickerTop,
+				left: pickerLeft
+			});
 		},
 		
 		update: function(newDate){
@@ -320,6 +349,7 @@
 								date: this.date,
 								viewMode: DPGlobal.modes[this.viewMode].clsName
 							});
+							this.hide();  //点击完日期后隐藏弹出框
 						}
 						break;
 				}
@@ -339,16 +369,15 @@
 		}
 	};
 	
-	$.fn.datepicker = function ( option ,container) {
+	$.fn.datepicker = function ( option, scrollEle ) {   //第二个参数val  暂时不确定val的作用，先删除.
 		return this.each(function () {
 			var $this = $(this),
 				data = $this.data('datepicker'),
-				options = typeof option === 'object' && option;	
-				container = container ? container : 'body';
+				options = typeof option === 'object' && option;
 			if (!data) {
-				$this.data('datepicker', (data = new Datepicker(this, $.extend({}, $.fn.datepicker.defaults,options),container)));
+				$this.data('datepicker', (data = new Datepicker(this, $.extend({}, $.fn.datepicker.defaults,options),scrollEle)));
 			}
-			/*if (typeof option === 'string') data[option](val);*/
+			// if (typeof option === 'string') data[option](val);
 		});
 	};
 
