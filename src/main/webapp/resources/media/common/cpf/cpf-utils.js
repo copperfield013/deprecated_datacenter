5ff34101c1a4aab47245b64a75662a7984082395
kfzhu <kfzhu@803ee233-1bbf-4b9d-9566-d4e45d14bbbd>
2017-12-04 04:27:16 +0000

    git-svn-id: http://47.100.187.235:13932/svn/datacenter@150 803ee233-1bbf-4b9d-9566-d4e45d14bbbd

--- src/main/webapp/resources/media/common/cpf/cpf-utils.js
+++ src/main/webapp/resources/media/common/cpf/cpf-utils.js
@@ -276,14 +276,14 @@ define(function(require, exports){
 			SEQUENCE_MAP[key] = currentSeq + 1;
 			return currentSeq;
 		},
-		datepicker		: function($dom,container){
+		datepicker		: function($dom,scrollEle){
 			return $($dom).datepicker({
 				format		: 'yyyy-mm-dd',
 				weekStart	: 1,
 				daysOfWeek : [ '日', '一', '二', '三', '四', '五', '六' ],  
                 monthNames : [ '一月', '二月', '三月', '四月', '五月', '六月',  
                         '七月', '八月', '九月', '十月', '十一月', '十二月' ]
-			},container);
+			},scrollEle);
 		},
 		/**
 		 * 获取焦点
