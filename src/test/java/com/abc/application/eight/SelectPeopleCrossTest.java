59abb9666b61cce939bc4410904453c896b9d854
nqwang <nqwang@803ee233-1bbf-4b9d-9566-d4e45d14bbbd>
2017-12-14 01:18:07 +0000

    git-svn-id: http://47.100.187.235:13932/svn/datacenter@193 803ee233-1bbf-4b9d-9566-d4e45d14bbbd

--- src/test/java/com/abc/application/eight/SelectPeopleCrossTest.java
+++ src/test/java/com/abc/application/eight/SelectPeopleCrossTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.abc.application.FusitionContext;
+import com.abc.application.FusionContext;
 import com.abc.mapping.entity.Entity;
 import com.abc.panel.Discoverer;
 import com.abc.panel.PanelFactory;
@@ -23,56 +23,59 @@ import com.abc.query.entity.impl.EntitySortedPagedQuery;
 @ContextConfiguration(locations = "classpath*:spring-core.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SelectPeopleCrossTest {
-	private static Logger logger = Logger.getLogger(SelectPeopleCrossTest.class);
+	private static Logger logger = Logger
+			.getLogger(SelectPeopleCrossTest.class);
 
 	protected String mapperName = "baseinfoImport";
 	protected String familyDoctorMapper = "familydoctor";
-	protected String dictionaryMappingName="default_dm";
+	protected String dictionaryMappingName = "default_dm";
+
 	@Test
 	public void select() {
+		FusionContext context = new FusionContext();
+		context.setMappingName(mapperName);
+		context.setSource(FusionContext.SOURCE_COMMON);
+		context.setDictionaryMappingName(dictionaryMappingName);
+		List<Criteria> criterias = new ArrayList<Criteria>();
+		CriteriaFactory criteriaFactory = new CriteriaFactory(context);
+		LikeQueryCriteria like = criteriaFactory
+				.createLikeQueryCriteria("姓名","张");
+		criterias.add(like);
+		QueryCriteria common = criteriaFactory.createQueryCriteria("性别","男");
+		criterias.add(common);
+		InequalQueryCriteria inequal = criteriaFactory
+				.createInequalQueryCriteria("身份证号码","");
+		criterias.add(inequal);
 
-			List<Criteria> criterias = new ArrayList<Criteria>();
-			LikeQueryCriteria like = CriteriaFactory.createLikeQueryCriteria(mapperName);
-			like.setName("姓名");
-			like.setValue("张");
-			criterias.add(like);
-			QueryCriteria common = CriteriaFactory.createQueryCriteria(mapperName);
-			common.setName("性别");
-			common.setValue("男");
-			criterias.add(common);
-			InequalQueryCriteria inequal = CriteriaFactory.createInequalQueryCriteria(mapperName);
-			inequal.setName("身份证号码");
-			inequal.setValue("");
-			criterias.add(inequal);
-			
-			select(criterias,"出生日期");
-		
+		select(criterias, "出生日期");
+
 	}
-	
+
 	@Test
 	public void selectNoQueryCondition() {
-			
-			select(null,null);
-		
+
+		select(null, null);
+
 	}
 
-	public void select(List<Criteria> criterias,String colName) {
+	public void select(List<Criteria> criterias, String colName) {
 		long startTime = System.currentTimeMillis();
 		try {
-			FusitionContext context=new FusitionContext();
+			FusionContext context = new FusionContext();
 			context.setMappingName(mapperName);
 			context.setDictionaryMappingName(dictionaryMappingName);
-			Discoverer discoverer=PanelFactory.getDiscoverer(context);
-			
-			EntitySortedPagedQuery sortedPagedQuery = discoverer.discover(criterias, colName);
+			Discoverer discoverer = PanelFactory.getDiscoverer(context);
+
+			EntitySortedPagedQuery sortedPagedQuery = discoverer.discover(
+					criterias, colName);
 			sortedPagedQuery.setPageSize(30);
-			
-			for (int i = 1; i < 4; i++) {
+
+			for (int i = 1; i < 2; i++) {
 				logger.debug("第" + i + "页,共" + sortedPagedQuery.getAllCount()
 						+ "条数据,每页" + sortedPagedQuery.getPageSize() + "条");
-//				abcNode.selectAliasAsTitle();
+				// abcNode.selectAliasAsTitle();
 				for (Entity entity : sortedPagedQuery.visit(i)) {
-//					people.addMapping(abcNode);
+					// people.addMapping(abcNode);
 					logger.debug(entity.toJson());
 				}
 			}
@@ -84,5 +87,5 @@ public class SelectPeopleCrossTest {
 			e.printStackTrace();
 		}
 
-	}	
+	}
 }
